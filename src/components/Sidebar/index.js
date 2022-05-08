1d628139884ccd36c309e9fa5f5f5c4ba756a3ca
Jonathan Perry <jonathan.perry@giveandtakeinc.com>
2022-05-08 15:58:04 -0400

    Added styled sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,73 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import BowlLogo from '../../assets/images/bowl.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={BowlLogo} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="subtitle" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link "
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/jonathanpperry"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.github.com/jonathanpperry"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.youtube.com/channel/UCtAF7R4omKgg-AFDc5O3kFw"
+        >
+          <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
